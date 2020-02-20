0da04fef06a8607ca7329ec2ee19a1227435312d
Avery Wolf <averyw2@uw.edu>
2020-02-19 22:46:28 -0800

    basic layout and middle section work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MdControlPoint, MdSearch, MdCropDin } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <header>  
+    <div className='leftsection'>
+      <div className='sectioncontent1'>
+        <div className='title1'>
+          Calendar
+        </div>
+      </div>
     </div>
+    <div className='middlesection'>
+      <div className='title2'>
+          To-Do List
+          <MdSearch style={{color: 'gray', 'margin-left':100, height:25, width:25}} />
+          <MdControlPoint style={{'margin-left':5, height:25, width:25}} />
+      </div>
+      <div className='subtitle1'>
+        Upcoming
+      </div>
+      <div className='dates'>
+        Saturday, February 22nd
+      </div>
+      <div className='cardwrap'>
+        <MdCropDin style={{'margin-left':5, height:20, width:20}} />
+        <div className='card'>
+          <div className='subject'>
+            HCDE 438
+          </div>
+          Project Proposal
+        </div>
+      </div>
+    </div>
+    <div className='rightsection'>
+      <div className='title3'>
+          Notes
+      </div>
+    </div>
+  </header>
   );
 }
 
